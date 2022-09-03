d1edc3a507d9a70b3f302eb12832ad7e952422b5
Pavel Ast a.k.a Pong <pavel.astashkin.05@mail.ru>
2022-09-03 08:24:33 +0300

    Added first excercise

---
+++ 001_Cubes/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите натуральное число N");
+int N = int.Parse(Console.ReadLine() ?? "0");
+
+while (N < 1)
+{
+    Console.WriteLine("Введено неправильное значение N, введите натуральное число N");
+    N = int.Parse(Console.ReadLine() ?? "0");
+}
+
+if (N == 1) Console.WriteLine($"Похоже что 1 единсвенный куб");
+
+for (int i = 1; i <= N; i += 1)
+{
+    Console.Write($"{Math.Pow(i, 3)} ");
+}
