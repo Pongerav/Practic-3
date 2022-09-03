3a44a16cb472b686de7a9370d5840de1093f935d
Pavel Ast a.k.a Pong <pavel.astashkin.05@mail.ru>
2022-09-03 08:57:23 +0300

    Added exercise 5

---
+++ 005_Chetn_Cubes/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите натуральное число N");
+int N = int.Parse(Console.ReadLine() ?? "0");
+
+while (N < 1)
+{
+    Console.WriteLine("Введено неправильное значение N, введите натуральное число N");
+    N = int.Parse(Console.ReadLine() ?? "0");
+}
+
+if (N == 1) Console.WriteLine("Похоже что чисел заканчивающихся на чётную цифру нет");
+
+for (int i = 2; i <= N; i += 2)
+{
+    Console.Write($"\t{ Math.Pow(i,3) }");
+}
