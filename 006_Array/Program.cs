3ee0186682bea2c39de17047039aaf3ff06b9978
Pavel Ast a.k.a Pong <pavel.astashkin.05@mail.ru>
2022-09-03 09:05:45 +0300

    Added program 006

---
+++ 006_Array/Program.cs
@@ -0,0 +1,18 @@
+﻿void FillArray(int[] array, int length)
+{
+    for (int i = 0; i < length; i += 1)
+    {
+        array[i] = new Random().Next(0,2);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    foreach (int i in array) Console.Write(i + " ");
+}
+
+
+int[] array = new int[8];
+
+FillArray(array, array.Length);
+PrintArray(array);
