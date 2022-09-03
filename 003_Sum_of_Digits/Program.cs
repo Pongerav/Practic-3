cacda256f5fcae1c3e3c6491e7eedfba4ea1db43
Pavel Ast a.k.a Pong <pavel.astashkin.05@mail.ru>
2022-09-03 08:41:49 +0300

    Added thirs exersice

---
+++ 003_Sum_of_Digits/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите целое число А");
+int A = int.Parse( Console.ReadLine() ?? "0");
+int x = A;
+int sum = 0;
+int digit = 0;
+
+while (x > 0) 
+{
+    digit = x % 10;
+    sum += digit;
+    x = x / 10;
+}
+
+Console.WriteLine($"Сумма цифр в числе равна {sum}");
